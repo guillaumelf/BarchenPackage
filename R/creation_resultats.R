de0a1a418902cb0b4b7d9109c95dd9a8ce9d21de
guillaumelf <guillaume.lefloch56@gmail.com>
2017-08-04 13:54:52 +0200

    final version

--- R/creation_resultats.R
+++ R/creation_resultats.R
@@ -5,6 +5,7 @@
 #' @param succes A data.table object with the successful attempts at the AMF exam.
 #' @param echecs A data.table object with the failed attempts at the AMF exam..
 #' @param resultats_vi A data.table object with the results of the "Validation Interne" exam.
+#' @param users A data.table object which contains informations about the userid and name of an individual.
 #' @return A unique dataset including the scores, final result, inscription and exam's date. This is returned as a data.table object.
 #' @note It is important to leave spaces in the condition as the string is splitted on this pattern. Look at the next section for examples of valid conditions.
 #' @examples
@@ -13,9 +14,17 @@
 #' echec <- data.table(read_excel(paste0(path,"envoiEchecs.xlsm"), sheet = "Feuil1", col_names = TRUE, skip = 1))
 #' res_vi <- data.table(read_excel(paste0(path,"Extraction Résultats Validation Interne.xlsx")))
 #' resultats <- create_results(succes,echecs,resultats_vi)
+#' tail(resultats,3)
+#' 
+#' ## This is was you'll get :
+#' ##   userid  %A %C resultat         inscription     examen
+#' ##1:  49615  47 43        0 2017-04-14 10:17:56 2017-04-19
+#' ##2:  50405  88 88        1 2017-05-18 13:20:29 2011-10-01
+#' ##3:  51183 100 91        1 2017-06-15 15:55:02 2014-08-20
+#' @note Lines with missing values will be omitted automatically.
 #' @export
 
-create_results <- function(succes,echecs,resultats_vi){
+create_results <- function(succes,echec,resultats_vi,users){
   
   # Il faut retraiter les variables de pourcentage de réussite qui sont en caractères
 
@@ -35,8 +44,8 @@ create_results <- function(succes,echecs,resultats_vi){
                      `%C`)]
   names(echec) <- c("nom","prenom","date","%A","%C")
   
-  succes[, `J session` := as.character(`J session`)][, `J session` := ifelse(str_length(`J session`) < 2, paste0("0",`J session`),`J session`)]
-  succes[, `M session` := as.character(`M session`)][, `M session` := ifelse(str_length(`M session`) < 2, paste0("0",`M session`),`M session`)]
+  succes[, `J session` := as.character(`J session`)][, `J session` := ifelse(stringr::str_length(`J session`) < 2, paste0("0",`J session`),`J session`)]
+  succes[, `M session` := as.character(`M session`)][, `M session` := ifelse(stringr::str_length(`M session`) < 2, paste0("0",`M session`),`M session`)]
   succes <- succes[, .(tolower(nom),
                        tolower(prenom),
                        as.IDate(paste(`A session`, `M session`, `J session`, sep ="-")),
@@ -67,7 +76,6 @@ create_results <- function(succes,echecs,resultats_vi){
   # Il reste maintenant à joindre le numéro d'identifiant userid à chaque candidat. On va faire la jointure sur la clé nom, prénom
   # On va donc modifier la casse pour éviter les soucis
   
-  names(users)
   users <- users[, .(id,tolower(lastname),tolower(firstname),as.POSIXct(firstaccess, origin = "1970-01-01"))]
   names(users) <- c("userid","nom","prenom","inscription")
   
@@ -92,5 +100,9 @@ create_results <- function(succes,echecs,resultats_vi){
   
   resultats[, first_attempt := min(date), by = userid]
   resultats <- resultats[date == first_attempt][, first_attempt := NULL]
+  resultats <- na.omit(resultats)
+  resultats <- resultats[!(stringr::str_detect(inscription,"1970"))]
+  resultats <- resultats[, .(userid,`%A`,`%C`,resultat,inscription,date)]
+  names(resultats) <- c("userid","%A","%C","resultat","inscription","examen")
   return(resultats)
 }
