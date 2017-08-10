deba3339db3f45b9ffe668b084e1df794d9ef38c
guillaumelf <guillaume.lefloch56@gmail.com>
2017-08-10 11:57:39 +0200

    mid-day update

--- R/clustering.R
+++ R/clustering.R
@@ -3,6 +3,7 @@
 #' This function will perform a clustering from PCA results.
 #'
 #' @param tab A data.table object containing the data on which the PCA has been performed.
+#' @param tab2 A data.table object containing variables you want to add after performing the clustering for the description in each group.
 #' @param results A list with the results of a PCA.
 #' @param nb.max.clusters A numeric indicating the maximum number of clusters you want to have. Default value is 10.
 #' @param size.min A numeric indicating how many individuals a cluster must contain at least. Default value is 30.
@@ -12,9 +13,10 @@
 #' 
 #' donnees <- clustering(res_pca,nb.max.clusters = 8, size.min = 20)
 #' @note  Clusters with a size inferior to what is specified inside the argument \code{size.min} will be removed.
+#' Results will be added only after the clustering is done, as it is a non-supervised one.
 #' @export
 
-clustering <- function(tab,results,nb.max.clusters = 10,size.min = 30){
+clustering <- function(tab,tab2,results,nb.max.clusters = 10,size.min = 30){
   res_clust <- FactoMineR::HCPC(results,nb.clust = nb.max.clusters, graph = FALSE)
   cluster <- c()
   size <- c()
@@ -25,11 +27,15 @@ clustering <- function(tab,results,nb.max.clusters = 10,size.min = 30){
   res <- data.table(cluster,size)
   retenus <- res[size >= size.min]$cluster
   retenus <- as.character(retenus)
-  userid <- row.names(tab)
-  donnees <- data.table(tab,cluster = res_clust$data.clust$clust)
-  row.names(donnees) <- userid
+  donnees <- data.table(tab,cluster = res_clust$data.clust$clust,keep.rownames = TRUE)
   donnees <- donnees[cluster %in% retenus]
   donnees$cluster <- factor(donnees$cluster)
+  row.names(donnees) <- donnees$rn
+  donnees[, userid := as.character(rn)][, rn := NULL]
+  tab2$userid <- as.character(tab2$userid)
+  liste <- list(donnees,tab2)
+  donnees <- left_join(list_tab = liste)
+  donnees[, userid := NULL]
   return(donnees)
 }
 
@@ -38,7 +44,6 @@ clustering <- function(tab,results,nb.max.clusters = 10,size.min = 30){
 #' This function will change the cluster labels.
 #'
 #' @param tab A data.table object containing the cluster labels.
-#' @param results A list with the results of a PCA.
 #' @return A data.table with the new cluster labels.
 #' @examples
 #' ## You perform a clustering and get these groups :
@@ -65,7 +70,7 @@ change_labels <- function(tab){
 #' This function will perform a barplot to represent each cluster's size.
 #'
 #' @param tab A data.table or data.frame object containing the data with clusters.
-#' @return A dataframe with original data and the clusters associated to each individual.
+#' @return The barplot representing the results.
 #' @export
 
 plot.clusters <- function(tab){
