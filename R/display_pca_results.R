322b4348bfd8cb1b315daac44b0c04a722f89849
guillaumelf <guillaume.lefloch56@gmail.com>
2017-08-11 09:59:21 +0200

    new function

--- R/display_pca_results.R
+++ R/display_pca_results.R
@@ -1,29 +1,23 @@
 #' Displays PCA results
 #'
-#' This function will plot the factorial plan of your choice as well as the table with contribution (CTR) and \code{COS²} of each individual on the correspondant axis.
+#' This function will display the table with contribution (CTR) and \code{COS²} of each individual on the axis of your choice.
 #'
 #' @param res_pca A list containing the PCA results.
-#' @param axe1 A numeric indicating the first axis you want to use to plot the factorial plan. Default value is 1.
-#' @param axe2 A numeric indicating the second axis you want to use to plot the factorial plan. Default value is 2.
-#' @return Plots the factorial plan you chose as well as PCA results for these axis.
+#' @param axe A numeric indicating the first axis you want to use to plot the factorial plan. Default value is 1.
+#' @return A data.frame object with the results of the axis.
 #' @examples
-#' ## You want to see the results on the first factorial plan
+#' ## You want to see the results of the first axis :
 #' 
-#' display_pca_results(res_pca, axe1 = 1, axe2 = 2)
+#' display_pca_results(res_pca, axe = 1)
 #' @export
 
-display_pca_results <- function(res_pca, axe1 = 1, axe2 = 2){
-  FactoMineR::plot.PCA(res_pca,axes = c(axe1,axe2), choix = "ind")
+display_pca_results <- function(res_pca, axe = 1){
   
   axe1_ind <- data.frame(CTR = res_pca$ind$contrib[,axe1], COS2 = res_pca$ind$cos2[,axe1], coord = res_pca$ind$coord[,axe1])
   axe1_ind <- data.table(axe1_ind, keep.rownames=TRUE)
   axe1_ind[, seuil := 1/nrow(axe1_ind)]
   axe1_ind <- axe1_ind[CTR > seuil][order(-CTR)]
-  View(axe1_ind)
-  
-  axe2_ind <- data.frame(CTR = res_pca$ind$contrib[,axe2], COS2 = res_pca$ind$cos2[,axe2], coord = res_pca$ind$coord[,axe2])
-  axe2_ind <- data.table(axe2_ind, keep.rownames=TRUE)
-  axe2_ind[, seuil := 1/nrow(axe2_ind)]
-  axe2_ind <- axe2_ind[CTR > seuil][order(-CTR)]
-  View(axe2_ind)
+  axe1_ind[, seuil := NULL]
+  axe1_ind <- as.data.frame(axe1_ind)
+  return(axe1_ind)
 }
