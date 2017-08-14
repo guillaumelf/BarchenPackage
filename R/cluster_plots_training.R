bfd30cdd7aee29376a5e34c47196c6f1bfd0ad6d
guillaumelf <guillaume.lefloch56@gmail.com>
2017-08-14 15:05:26 +0200

    v2

--- R/cluster_plots_training.R
+++ R/cluster_plots_training.R
@@ -33,6 +33,6 @@ affiche_tr_attempt <- function(tab,phases = 5){
              width=0.5, colour="grey40", size=0.4, stat = "identity") + 
     scale_fill_discrete(drop=FALSE) +
     labs(x="phases",y="") + ggtitle("Evolution au cours du temps du nombre \n moyen de tentatives effectuées par session") +
-    theme(plot.title = element_text(hjust = 0.5),legend.title = element_blank()) + scale_fill_manual(values=c("#F97400")) +
+    theme(plot.title = element_text(hjust = 0.5),legend.position = 'none') + scale_fill_manual(values=c("#F97400")) +
     facet_wrap(~cluster,nrow=2)
 }
